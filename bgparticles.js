a5ddab1cae56945bbfff8732a23db1b158b693a9
reversedcircularity <librorumprohibitorumi@gmail.com>
2025-12-29 12:53:04 +0800

    another particle layer

--- bgparticles.js
+++ bgparticles.js
@@ -1,4 +1,27 @@
-tsParticles.load("bgparticles", {
+tsParticles.load("bgparticles-back", {
+    particles: {
+        number: {
+            value: 200,
+        },
+        move: {
+            enable: true,
+        },
+        links: {
+            enable: true,
+            distance: 250,
+            color: "0000ff",
+            opacity: 0.5,
+        },
+        color: {
+            value: "0000ff",
+        },
+        size: {
+            value: { min: 1, max: 3 },
+        },
+    },
+});
+
+tsParticles.load("bgparticles-front", {
     particles: {
         number: {
             value: 200,
@@ -10,6 +33,7 @@ tsParticles.load("bgparticles", {
             enable: true,
             distance: 250,
             color: "0088ff",
+            opacity: 1,
         },
         color: {
             value: "00ffff",
